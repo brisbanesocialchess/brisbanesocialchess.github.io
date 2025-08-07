5ae926d70c9abef642c3c0639895fbe4ea335239
Max Base <MaxBaseCode@gmail.com>
2025-08-07 15:36:58 +0330

    remove comments from config js

--- .eleventy.js
+++ .eleventy.js
@@ -2,25 +2,20 @@ const { DateTime } = require("luxon");
 const slugify = require("slugify");
 
 module.exports = function(eleventyConfig) {
-  // Date filter for formatting dates nicely
   eleventyConfig.addFilter("date", (dateObj, format = "yyyy-MM-dd") => {
     return DateTime.fromJSDate(dateObj).toFormat(format);
   });
 
-  // Slug filter for URL-safe strings (used in category/tag URLs)
   eleventyConfig.addFilter("slug", (str) => {
     return slugify(str, { lower: true, remove: /[*+~.()'"!:@]/g });
   });
 
-  // Passthrough copy for assets folder
   eleventyConfig.addPassthroughCopy("assets");
 
-  // Collection: all posts inside /posts folder, sorted by date descending
   eleventyConfig.addCollection("posts", function(collectionApi) {
     return collectionApi.getFilteredByGlob("posts/*.md").sort((a, b) => b.date - a.date);
   });
 
-  // Collection: unique categories from posts only
   eleventyConfig.addCollection("categories", function(collectionApi) {
     let categories = new Set();
     collectionApi.getFilteredByGlob("posts/*.md").forEach(item => {
@@ -31,7 +26,6 @@ module.exports = function(eleventyConfig) {
     return [...categories];
   });
 
-  // Collection: unique tags from posts only
   eleventyConfig.addCollection("tags", function(collectionApi) {
     let tags = new Set();
     collectionApi.getFilteredByGlob("posts/*.md").forEach(item => {
