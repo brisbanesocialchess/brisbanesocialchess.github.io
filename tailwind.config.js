79cd329f9ee6b85ddf4746bd56b88f0ee8d60b4f
Max Base <MaxBaseCode@gmail.com>
2025-08-28 19:16:56 +0330

    remove test index.html, port from v3 to 4

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,6 @@ module.exports = {
     './_site/**/*.{html}',
   ],
   safelist: [
-    'bg-table-header',
     'role-admin',
     'role-developer',
     'role-moderator',
@@ -17,36 +16,6 @@ module.exports = {
     'role-event-organizers',
     'role-assistant-organizers',
   ],
-  theme: {
-    extend: {
-      colors: {
-        'table-header': '#091a52',
-        'role-admin': '#ff0000',
-        'role-developer': '#00ffff',
-        'role-moderator': 'yellow',
-        'role-board': '#ed7014',
-        'role-coordinator': '#a020f0',
-        'role-co-organizers': '#2196f3',
-        'role-lead-developer': '#ff0001',
-        'role-event-organizers': '#00d166',
-        'role-assistant-organizers': '#c35339',
-      },
-    },
-  },
-  plugins: [
-    function({ addBase, theme }) {
-      const roles = [
-        'admin','developer','moderator','board','coordinator',
-        'co-organizers','lead-developer','event-organizers','assistant-organizers'
-      ];
-      const base = {};
-
-      roles.forEach(role => {
-        const color = theme(`colors.role-${role}`);
-        base[`.role-${role} img`] = { border: `1px solid ${color}` };
-      });
-
-      addBase(base);
-    }
-  ],
+  theme: {},
+  plugins: [],
 };
