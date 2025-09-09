3b182aeae6dd5936b2b6505c9e22f46d826dcd0b
Max Base <MaxBaseCode@gmail.com>
2025-09-09 23:31:50 +0330

    Move template assets to images, and move content images to pictures

--- vite.config.js
+++ vite.config.js
@@ -2,24 +2,18 @@ import { defineConfig } from 'vite';
 import postcssImport from 'postcss-import';
 
 export default defineConfig({
-	root: './_site',
-	build: {
-		emptyOutDir: true,
-		outDir: '../_build',
-		rollupOptions: {
-			input: './_site/assets/main.css',
-			output: {
-				assetFileNames: 'assets/style.css'
-			},
-		},
-	},
-	css: {
-		postcss: {
-			plugins: [postcssImport()],
-		},
-	},
-	server: {
-		port: 5173,
-		strictPort: true,
-	},
+  root: './_site',
+  build: {
+    emptyOutDir: true,
+    outDir: '../_build/',
+  },
+  css: {
+    postcss: {
+      plugins: [postcssImport()],
+    },
+  },
+  server: {
+    port: 5173,
+    strictPort: true,
+  },
 });
