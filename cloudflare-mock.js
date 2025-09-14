310c304ba4654fcfccdc524ffa6ffde8029fdf11
Seyyed Ali Mohammadiyeh (MAX BASE) <MaxBaseCode@gmail.com>
2025-09-14 14:40:43 +0330

    update cloudflare mock

--- cloudflare-mock.js
+++ cloudflare-mock.js
@@ -1,4 +1,24 @@
 export const env = {};
-export function createExecutionContext() { return {}; }
-export function waitOnExecutionContext() {}
-export const SELF = {};
+
+export function createExecutionContext() {
+  return {
+    passThroughOnException: () => {},
+    waitUntil: async (promise) => await promise,
+  };
+}
+
+export function waitOnExecutionContext() {
+  return Promise.resolve();
+}
+
+export const SELF = {
+  fetch: async (req) => {
+    const { default: worker } = await import('../src/index.js');
+
+    if (typeof worker.fetch === 'function') {
+      return worker.fetch(req, env, createExecutionContext());
+    }
+
+    return new Response('No fetch() defined', { status: 500 });
+  },
+};
