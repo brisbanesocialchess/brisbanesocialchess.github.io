40b24346e4efc75426540743d25f901914fa6c5e
Seyyed Ali Mohammadiyeh (MAX BASE) <MaxBaseCode@gmail.com>
2025-09-14 10:57:28 +0330

    Add exclude for coverage

--- vite.config.js
+++ vite.config.js
@@ -53,6 +53,16 @@ export default defineConfig({
 			provider: 'v8',
 			reporter: ['text', 'html', 'cobertura'],
 			reportsDirectory: './coverage',
+			exclude: [
+				'**/node_modules/**',
+				'**/dist/**',
+				'**/cypress/**',
+				'**/.{idea,git,cache,output,temp}/**',
+				'**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*',
+				'**/coverage/**',
+				'**/_site/**',
+				'**/_deploy/**',
+			],
 		},
 	},
 });
